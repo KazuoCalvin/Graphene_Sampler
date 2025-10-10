c8821e91b40e2ca23892b99ebd6b267aeb5e58e1
Kazu Nakane <kazuhiko.nakane@gmail.com>
2025-10-10 16:52:38 -0700

    structured dir for pyinstaller

---
+++ src/app/main.py
@@ -0,0 +1,11 @@
+from PyQt5.QtWidgets import QApplication
+
+import sys
+
+def main():
+    app = QApplication(sys.argv)
+    # Your main window or application logic here
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
